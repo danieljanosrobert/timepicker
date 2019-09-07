9808afdc0cdbd75ba50b8d6c198edf5c3f40b04f
Daniel János Róbert <h650011@stud.u-szeged.hu>
2019-09-08 00:25:26 +0200

    refs#6 - parallax, menu komponens

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,17 @@
 <template>
-  <HelloWorld />
+  <v-container fluid class="full-height pa-0">
+    <HomeParallax/>
+    TEXT FOR HOMEPAGE BECAUSE WITHOUT IT THERE IS NO MENU
+  </v-container>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-import HelloWorld from '../components/HelloWorld.vue';
+import HomeParallax from '@/components/HomeParallax.vue';
 
 export default Vue.extend({
   components: {
-    HelloWorld,
+    HomeParallax,
   },
 });
 </script>
