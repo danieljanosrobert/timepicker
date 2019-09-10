24212c0be9e7f0988e312670a523bd55abe18ebe
Daniel János Róbert <h650011@stud.u-szeged.hu>
2019-09-10 22:35:28 +0200

    refs#5 - Homepage fejlesztése

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container fluid class="pa-0">
     <HomeParallax/>
-    <HomeSimple/>
+    <HomeSimple ref="home-simple"></HomeSimple>
   </v-container>
 </template>
 
@@ -14,6 +14,12 @@ export default Vue.extend({
   components: {
     HomeParallax,
     HomeSimple,
+  },
+  methods: {
+    scrollTo(refName: string): void {
+      const element: number = ((this.$refs[refName]) as Vue & { $el: HTMLElement}).$el.offsetTop;
+      window.scrollTo(0, element);
+    },
   },
 });
 </script>
