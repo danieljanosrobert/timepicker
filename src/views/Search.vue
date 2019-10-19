0f86957cd77b3528e49e0e089f123ae468af3534
Daniel János Róbert <h650011@stud.u-szeged.hu>
2019-10-20 01:59:24 +0200

    refs#8 - kereses hozzaadasa

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,13 +1,29 @@
 <template>
-  
+  <v-container fluid id="container" class="pa-0">
+    <v-col xl="8" offset-xl="2"
+           md="10" offset-md="1"
+           xs="12" offset-xs="0"
+    class="pa-0">
+      <DataCards/>
+      <DataTable/>
+    </v-col>
+  </v-container>
 </template>
 
 <script>
+  import DataTable from '@/components/search/DataTable.vue';
+  import DataCards from '@/components/search/DataCards.vue';
+
   export default {
-    name: "Search",
+    name: 'Search',
+    components: {DataCards, DataTable},
   };
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+  @import '@/assets/styles/variables.scss';
 
+  #container {
+    background-color: $_yellow;
+  }
 </style>
